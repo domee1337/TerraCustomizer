12c4f5a5ebb53bdd965aa7de3c16a19575704dae
Dominik Jödecke <a2@fs24.local>
2018-01-27 07:56:12 +0100

    x

---
+++ src/Providers/TerraCustomizerServiceProvider.php
@@ -0,0 +1,24 @@
+<?php
+namespace TerraCustomizer\Providers;
+
+use IO\Helper\TemplateContainer;
+use IO\Helper\ResourceContainer;
+use Plenty\Plugin\Events\Dispatcher;
+use Plenty\Plugin\ServiceProvider;
+use Plenty\Plugin\Templates\Twig;
+
+/**
+ * Class TerraCustomizerServiceProvider
+ * @package TerraCustomizer\Providers
+ */
+class TerraCustomizerServiceProvider extends ServiceProvider
+{
+
+	/**
+	 * Register the service provider.
+	 */
+	public function register()
+	{
+		
+	}
+}
