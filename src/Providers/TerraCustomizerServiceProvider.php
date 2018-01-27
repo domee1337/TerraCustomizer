e6242c32b1a11a049d478e40379bc440ca81aeae
Dominik Jödecke <a2@fs24.local>
2018-01-27 08:02:44 +0100

    sad

--- src/Providers/TerraCustomizerServiceProvider.php
+++ src/Providers/TerraCustomizerServiceProvider.php
@@ -1,11 +1,7 @@
 <?php
 namespace TerraCustomizer\Providers;
 
-use IO\Helper\TemplateContainer;
-use IO\Helper\ResourceContainer;
-use Plenty\Plugin\Events\Dispatcher;
 use Plenty\Plugin\ServiceProvider;
-use Plenty\Plugin\Templates\Twig;
 
 /**
  * Class TerraCustomizerServiceProvider
